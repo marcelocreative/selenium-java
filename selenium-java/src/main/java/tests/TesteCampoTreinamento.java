11e00e2e6e7a950699ccfaed3934b2356a745db9
marcelocreative <marcelolnoliveira@gmail.com>
2022-07-22 19:36:12 -0300

    Elementos básicos: TextField e TextArea

--- selenium-java/src/main/java/tests/TesteCampoTreinamento.java
+++ selenium-java/src/main/java/tests/TesteCampoTreinamento.java
@@ -1,13 +1,15 @@
 package tests;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoTreinamento {
 	
 	@Test
-	public void teste() {
+	public void testeTextField() {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\marce\\drivers\\chromedriver.exe");
 		
@@ -15,8 +17,27 @@ public class TesteCampoTreinamento {
 		driver.manage().window().maximize();
 		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/resources//componentes.html");
 		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Escrita de texto");
+		Assert.assertEquals("Escrita de texto", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		
+		driver.quit();
 		
 	}
+	
+	@Test
+	public void deveInteragirComTextArea() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\marce\\drivers\\chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/resources//componentes.html");
+		
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Texto no textArea");
+		Assert.assertEquals("Texto no textArea", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		
+		driver.quit();
+	}
+
 
 }
